72bce6cec2312d45110ecc870f220437ccf4af6f
carolinafrederico <iam@carolinafrederico.com>
2025-03-29 01:42:45 -0400

    Description and titles fixed

--- script.js
+++ script.js
@@ -2,46 +2,52 @@ const apiKey = '7ba4a580810a4bde8575f8bdd4205c7d'
 
 const blogContainer = document.getElementById("blog-container");
 
-async function fetchRandomNews () {
+async function fetchRandomNews() {
     try {
-        const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=12&apiKey=${apiKey}`
+        const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apiKey=${apiKey}`
         const response = await fetch(apiUrl);
         const data = await response.json();
-        return data.articles; 
-        
+        return data.articles;
+
     } catch (error) {
         console.error("Error fetching random news", error)
         return []
-        
+
     }
 }
 
-function displayBlogs (articles){
-    blogContainer.innerHTML = ""
+function displayBlogs(articles) {
+    blogContainer.innerHTML = "";
     articles.forEach((article) => {
-        const blogCard = document.createElement("div")
-        blogCard.classList.add("blog-card")
-        const img = document.createElement("img")
-        img.src = article.urlToImage
-        img.alt = article.title
-        const title = document.createElement("h2")
-        title.textContent = article.title
-        const description = document.createElement("p")
-        description.textContent = article.description
-        blogCard.appendChild(img)
-        blogCard.appendChild(title)
-        blogCard.appendChild(description)
-        blogContainer.appendChild(blogCard)
+        const blogCard = document.createElement("div");
+        blogCard.classList.add("blog-card");
+        const img = document.createElement("img");
+        img.src = article.urlToImage;
+        img.alt = article.title;
+        const title = document.createElement("h2");
+        const truncatedTitle = article.title.length > 51 ? article.title.slice(0, 51) + "..." : article.title;
+        title.textContent = article.title;
+        title.textContent = truncatedTitle;
+        const description = document.createElement("p");
+        const truncatedDes = article.description.length > 120 ? article.description.slice(0, 120) + "..." : article.description;
+        description.textContent = truncatedDes;
+        blogCard.appendChild(img);
+        blogCard.appendChild(title);
+        blogCard.appendChild(description);
+        blogCard.addEventListener("click", () => {
+            window.open(article.url, "_blank");
+        });
+        blogContainer.appendChild(blogCard);
 
-    });        
+    });
 }
 
-(async() => {
+(async () => {
     try {
         const articles = await fetchRandomNews()
         displayBlogs(articles);
     } catch (error) {
-        console.error("Error fetching random news", error );
-        
+        console.error("Error fetching random news", error);
+
     }
 })();

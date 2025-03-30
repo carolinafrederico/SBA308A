f7aa2eb0e59b6cd7d1a05af9aa21429146bab480
carolinafrederico <iam@carolinafrederico.com>
2025-03-30 00:26:33 -0400

    Search box working

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const apiKey = '7ba4a580810a4bde8575f8bdd4205c7d'
 
 const blogContainer = document.getElementById("blog-container");
+const searchField = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
 
 async function fetchRandomNews() {
     try {
-        const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apiKey=${apiKey}`
+        const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=12&apiKey=${apiKey}`
         const response = await fetch(apiUrl);
         const data = await response.json();
         return data.articles;
@@ -13,6 +15,32 @@ async function fetchRandomNews() {
         console.error("Error fetching random news", error)
         return []
 
+    }
+}
+
+searchButton.addEventListener("click", async () =>{
+    const query = searchField.value.trim()
+    if (query !== ""){
+        try {
+            const articles = await fetchNewsQuery(query) 
+            displayBlogs (articles)
+        } catch (error) {
+            console.log("Error fetching news by query", error)
+        }
+    }
+
+})
+
+async function fetchNewsQuery (query){
+    try {
+        const apiUrl = `https://newsapi.org/v2/everything?q=${query}&pageSize=12&apiKey=${apiKey}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data.articles;
+    } catch (error) {
+        console.error("Error fetching random news", error);
+        return [];
+        
     }
 }
 
@@ -26,7 +54,7 @@ function displayBlogs(articles) {
         img.alt = article.title;
         const title = document.createElement("h2");
         const truncatedTitle = article.title.length > 51 ? article.title.slice(0, 51) + "..." : article.title;
-        title.textContent = article.title;
+        // title.textContent = article.title;
         title.textContent = truncatedTitle;
         const description = document.createElement("p");
         const truncatedDes = article.description.length > 120 ? article.description.slice(0, 120) + "..." : article.description;
